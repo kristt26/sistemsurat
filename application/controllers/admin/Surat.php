ec7b107f7426ef0b4beb2411fa5e680151b3eb12
kristt26 <kristt26@gmail.com>
2020-09-05 06:04:24 +0900

    ds

--- application/controllers/admin/Surat.php
+++ application/controllers/admin/Surat.php
@@ -11,6 +11,8 @@ class Surat extends CI_Controller
         parent::__construct();
         $this->load->model('Surat_model');
         $this->load->library('mylib');
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, OPTIONS");
         // if (!$this->session->userdata("logged_in")) {
         //     redirect('auth');
         // }
