6171b08b755082131f3bbd59d6ab424b8e035047
kristt26 <kristt26@gmail.com>
2020-10-10 22:59:27 +0900

    tele

--- application/controllers/Telegram.php
+++ application/controllers/Telegram.php
@@ -23,7 +23,7 @@ class Telegram extends CI_Controller
 
         $data = array(
             'chat_id' => $chatID,
-            'text' => $message === "getId" ? "Id Chat Anda: " . $chatID : "Perintah yang anda masukkan salah",
+            'text' => $message === "getId" || $message === "getid" ? "Id Chat Anda: " . $chatID : "Perintah yang anda masukkan salah",
             'reply_to_message_id' => $msgid,
 
         );
